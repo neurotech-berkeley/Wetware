94718cc46d9015219ef96511ac4eb5db3f916348
tavish-mishra <tavish.mishra@gmail.com>
2024-04-02 20:43:03 -0700

    did smth

--- SocketPrototypes/readData.py
+++ SocketPrototypes/readData.py
@@ -1,10 +1,16 @@
 import socket
+import numpy as np
 
 # Define the server's IP address and port
 server_ip = "127.0.0.1"  # Localhost
 server_port = 8080  # Example port number
 
-try:
+numChannels = 10
+bufferSize = 10
+
+def readBytes(numChannels, bufferSize):
+    result = np.empty(numChannels * bufferSize)
+    i = 0
     while True:
         # Create a TCP/IP socket
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,14 +18,23 @@ try:
         # Connect to the server
         client_socket.connect((server_ip, server_port))
 
-        # Receive data from the server
-        data = client_socket.recv(1024).decode()
+        data = client_socket.recv(bufferSize*numChannels*4*8)
 
-        # Print the received data
-        print(f"Received timestamp from server: {data}")
+        arr = []
+        # Receive data from the server
+        for i in range(numChannels):
+            temp = []
+            for j in range(bufferSize):
+                temp += [int.from_bytes(data[(i*numChannels*4)+j*4:(i*numChannels*4)+(j+1)*4], "little")]
+            arr += [temp]
+        
+        print(arr)
+
 
     # Close the socket
     client_socket.close()
 
-except Exception as e:
-    print("Exception:", str(e))
+
+    return result.reshape((numChannels, bufferSize))
+
+readBytes(10, 10)
