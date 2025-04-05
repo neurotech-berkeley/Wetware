69c76d3cb2d149d362e32795d2c5af25ed6e7e02
Deeksha Kasula <53919657+DK-123@users.noreply.github.com>
2025-04-04 20:31:54 -0700

    Add files via upload

---
+++ SocketPrototypes/MEASocketConnection/Form1.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Mcs.Usb;
+
+namespace MEA2100_Recording_and_Stimulation
+{
+    public partial class Form1 : Form
+    {
+        private CMcsUsbListNet list = new CMcsUsbListNet(DeviceEnumNet.MCS_DEVICE_USB);
+
+        private CMeaUSBDeviceNet dacq = new CMeaUSBDeviceNet();
+        private CStg200xDownloadNet stg = new CStg200xDownloadNet();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            list.DeviceArrival += List_DeviceArrivalRemoval;
+            list.DeviceRemoval += List_DeviceArrivalRemoval;
+
+            dacq.ErrorEvent += Dacq_ErrorEvent;
+            dacq.ChannelDataEvent += Dacq_ChannelDataEvent;
+
+            stg.Stg200xPollStatusEvent += Stg_Stg200xPollStatusEvent;
+
+            RefreshDeviceList();
+        }
+
+        private void Stg_Stg200xPollStatusEvent(uint status, StgStatusNet stgStatusNet, int[] index_list)
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new OnStgPollStatus(Stg_Stg200xPollStatusEvent), status, stgStatusNet, index_list);
+            }
+            else
+            {
+                listBox1.Items.Add("STG: " + status.ToString("X8"));
+            }
+        }
+
+        private void RefreshDeviceList()
+        {
+            cbDeviceList.Items.Clear();
+            cbDeviceList.Items.AddRange(list.GetUsbListEntries());
+            if (cbDeviceList.Items.Count > 0)
+            {
+                cbDeviceList.SelectedIndex = 0;
+            }
+        }
+
+        private void List_DeviceArrivalRemoval(CMcsUsbListEntryNet entry)
+        {
+            RefreshDeviceList();
+        }
+
+        private void Dacq_ErrorEvent(string msg, int action)
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new OnError(Dacq_ErrorEvent), msg, action);
+            }
+            else
+            {
+                listBox1.Items.Add(msg);
+            }
+        }
+
+        private int Electrode1 = 0;
+        private int Electrode2 = 1;
+        private int HeadStage = 3;
+
+        private int channelsInBlock = 0;
+        private int threshold = 0;
+        private void Dacq_ChannelDataEvent(CMcsUsbDacqNet dacq, int CbHandle, int numFrames)
+        {
+            List<int[]> data = new List<int[]>();
+            for (int i = 0; i < channelsInBlock / 2; i++)
+            {
+                data.Add(dacq.ChannelBlock.ReadFramesI32(i, 0, threshold, out int frames_ret));
+            }
+
+            if (!inHandleData) // skip if last invoke has not finished yet (weak graphic performance)
+            {
+                // 0 - 59: Elektrode Channels
+                // 60 - 67 IFB Analog IFB channels
+                // 68 Digital In/Out
+                // 69 - 74 Sideband channels
+                // 75 - 76 Checksum channels
+                BeginInvoke(new HandleDataDelegate(HandleData), data[Electrode1], data[69 + HeadStage]);
+            }
+        }
+
+        delegate void HandleDataDelegate(int[] data1, int[] data2);
+
+        // Used for weak graphic performance
+        private bool inHandleData = false;
+
+        private void HandleData(int[] data1, int[] data2)
+        {
+            inHandleData = true;
+
+            bool scaled = false;
+            if (scaled)
+            {
+                double[] scaledData = ScaleData(data1, resolutionHS);
+                FillSeries(0, scaledData);
+            }
+            else
+            {
+                FillSeries(0, data1);
+            }
+            FillSeries(1, data2);
+
+            inHandleData = false;
+        }
+
+        private void FillSeries(int serie, int[] data)
+        {
+            chart1.Series[serie].Points.Clear();
+            int length = data.Length;
+            int joined = length / chart1.Width;
+            for (int i = 0; i < length; i++)
+            {
+#if true // workaround for performance 
+                double x = i;
+                double max = Double.MinValue;
+                double min = Double.MaxValue;
+                for (int j = 0; j < joined && i < length; j++, i++)
+                {
+                    if (data[i] > max)
+                    {
+                        max = data[i];
+                    }
+
+                    if (data[i] < min)
+                    {
+                        min = data[i];
+                    }
+                }
+
+                if (max != Double.MinValue)
+                {
+                    chart1.Series[serie].Points.AddXY(x, max);
+                    chart1.Series[serie].Points.AddXY(x, min);
+                }
+#else
+                chart1.Series[serie].Points.AddXY(i, data[i]);
+#endif
+            }
+        }
+
+        private void FillSeries(int serie, double[] data)
+        {
+            chart1.Series[serie].Points.Clear();
+            int length = data.Length;
+            int joined = length / chart1.Width;
+            for (int i = 0; i < length; i++)
+            {
+#if true // workaround for performance 
+                double x = i;
+                double max = Double.MinValue;
+                double min = Double.MaxValue;
+                for (int j = 0; j < joined && i < length; j++, i++)
+                {
+                    if (data[i] > max)
+                    {
+                        max = data[i];
+                    }
+
+                    if (data[i] < min)
+                    {
+                        min = data[i];
+                    }
+                }
+
+                if (max != Double.MinValue)
+                {
+                    chart1.Series[serie].Points.AddXY(x, max);
+                    chart1.Series[serie].Points.AddXY(x, min);
+                }
+#else
+                chart1.Series[serie].Points.AddXY(i, data[i]);
+#endif
+            }
+        }
+
+        private double resolutionHS = 1;
+        private double resolutionIFBADC = 1;
+
+        double[] ScaleData(int[] data, double resolution)
+        {
+            double[] scaled = new double[data.Length];
+            for (int i = 0; i < data.Length; i++)
+            {
+                scaled[i] = resolution * (double) data[i];
+            }
+
+            return scaled;
+        }
+
+        private void btRecordingStart_Click(object sender, EventArgs e)
+        {
+            CMcsUsbListEntryNet deviceEntry = (CMcsUsbListEntryNet) cbDeviceList.SelectedItem;
+            uint status = dacq.Connect(deviceEntry);
+            if (status == 0)
+            {
+                stg.Connect(deviceEntry);
+
+                dacq.StopDacq(0); // if software hat not stopped sampling correctly before
+
+                CSCUFunctionNet scu = new CSCUFunctionNet(dacq);
+                scu.SetDacqLegacyMode(false);
+
+                int samplerate = 50000;
+                dacq.SetSamplerate(samplerate, 0, 0);
+
+                dacq.SetDataMode(DataModeEnumNet.Signed_32bit, 0);
+
+                dacq.GetResolutionPerDigit(0, DacqGroupChannelEnumNet.HeadstageElectrodeGroup, out int resHS, out int resHSUnit);
+                resolutionHS = resHS * Math.Pow(10, -resHSUnit);
+
+                dacq.GetResolutionPerDigit(0, DacqGroupChannelEnumNet.InterfaceADCGroup, out int resIFBADC, out int resIFBADCUnit);
+                resolutionIFBADC = resIFBADC * Math.Pow(10, -resIFBADCUnit);
+
+
+                // for MEA2100-Mini it is assumed that only one HS is connected
+                dacq.SetNumberOfAnalogChannels(60, 0, 0, 8, 0);
+
+                dacq.EnableDigitalIn(DigitalDatastreamEnableEnumNet.DigitalIn | DigitalDatastreamEnableEnumNet.DigitalOut |
+                                     DigitalDatastreamEnableEnumNet.Hs1SidebandLow | DigitalDatastreamEnableEnumNet.Hs1SidebandHigh, 0);
+                dacq.EnableChecksum(true, 0);
+
+                // numbers are in 16bit
+                dacq.GetChannelLayout(out int analogChannels, out int digitalChannels, out int checksumChannels, out int timestampChannels, out channelsInBlock, 0);
+
+                int queuesize = samplerate;
+                threshold = samplerate / 10;
+                // channelsInBlock / 2 gives the number of channels in 32bit
+                bool[] sel = Enumerable.Repeat(true, channelsInBlock / 2).ToArray();
+                dacq.ChannelBlock.SetSelectedChannels(Enumerable.Repeat(true, channelsInBlock / 2).ToArray(), queuesize, threshold, SampleSizeNet.SampleSize32Signed, SampleDstSizeNet.SampleDstSize32, channelsInBlock);
+
+                dacq.ChannelBlock.SetCommonThreshold(threshold);
+
+                dacq.ChannelBlock.SetCheckChecksum((uint) checksumChannels, (uint) timestampChannels);
+
+                dacq.StartDacq();
+            }
+            else
+            {
+                MessageBox.Show("Connection failed: " + CMcsUsbNet.GetErrorText(status));
+            }
+        }
+
+        private void btRecordingStop_Click(object sender, EventArgs e)
+        {
+            dacq.StopDacq(0);
+            dacq.Disconnect();
+            stg.Disconnect();
+        }
+
+        private void btStimulationStart_Click(object sender, EventArgs e)
+        {
+            int[] amplitude1 = new int[] { 10000, -10000, 0, 20000, -20000, 0 };
+            int[] amplitude2 = new int[] { -10000, 10000, 0, -20000, 20000, 0 };
+            int[] sideband1 = new int[] { 1 << 8, 3 << 8, 0, 1 << 8, 3 << 8, 0 }; // user defined sideband (use bits > 8)
+            int[] StimulusActive1 = new int[] {1, 1, 0, 1, 1, 0};
+            int[] sideband2 = new int[] { 4 << 8, 12 << 8, 0, 4 << 8, 12 << 8, 0 }; // user defined sideband (use bits > 8)
+            int[] StimulusActive2 = new int[] { 1, 1, 0, 1, 1, 0 };
+            ulong[] duration = new ulong[] {1000, 1000, 10000, 1000, 1000, 100000}; // could be different in length and numbers for each amplitude and sideband, it only needs to be equal in length for the individual amplitude and sideband 
+
+            // Defining the Elektrodes. Choose if you want use automatic or manual mode for the stimulation electrode here:
+            ElectrodeModeEnumNet electrodeMode = ElectrodeModeEnumNet.emAutomatic;
+            //ElectrodeModeEnumNet electrodeMode = ElectrodeModeEnumNet.emManual;
+            for (int i = 0; i < 60; i++)
+            {
+                stg.SetElectrodeMode((uint)HeadStage, (uint)i, i == Electrode1 || i == Electrode2 ? electrodeMode : ElectrodeModeEnumNet.emAutomatic);
+                stg.SetElectrodeEnable((uint)HeadStage, (uint)i, 0, i == Electrode1 || i == Electrode2 ? true : false);
+                stg.SetElectrodeDacMux((uint)HeadStage, (uint)i, 0, 
+                    i == Electrode1 ? ElectrodeDacMuxEnumNet.Stg1 : 
+                    ( i == Electrode2 ? ElectrodeDacMuxEnumNet.Stg2 : ElectrodeDacMuxEnumNet.Ground));
+                stg.SetEnableAmplifierProtectionSwitch((uint)HeadStage, (uint)i, false); // Enable the switch if you want to protect the amplifier from an overload
+                stg.SetBlankingEnable((uint)HeadStage, (uint)i, false); // Choose if you want Filter blanking during stimulation for an electrode
+            }
+
+            stg.SetVoltageMode(0);
+
+            // bit0 (blanking switch) activation duration prolongation in µs
+            uint Bit0Time = 40;
+
+            // bit3 (stimulation switch) activation duration prolongation in µs
+            uint Bit3Time = 800;
+
+            // bit4 (stimulus selection switch) activation duration prolongation in µs
+            uint Bit4Time = 40;
+
+            // STG Channel 1:
+            stg.PrepareAndSendData(2 * (uint)HeadStage + 0, amplitude1, duration, STG_DestinationEnumNet.channeldata_voltage);
+
+            if (electrodeMode == ElectrodeModeEnumNet.emManual)
+            {
+                //pure user defined sideband:
+                stg.PrepareAndSendData(2 * (uint)HeadStage + 0, sideband1, duration, STG_DestinationEnumNet.syncoutdata);
+            }
+            else
+            {
+                //alternative: adding sideband data for automatic stimulation mode:
+                CStimulusFunctionNet.SidebandData SidebandData1 = stg.Stimulus.CreateSideband(StimulusActive1, sideband1, duration, Bit0Time, Bit3Time, Bit4Time);
+                stg.PrepareAndSendData(2 * (uint) HeadStage + 0, SidebandData1.Sideband, SidebandData1.Duration, STG_DestinationEnumNet.syncoutdata);
+            }
+
+            // STG Channel 2:
+            stg.PrepareAndSendData(2 * (uint)HeadStage + 1, amplitude2, duration, STG_DestinationEnumNet.channeldata_voltage);
+
+            if (electrodeMode == ElectrodeModeEnumNet.emManual)
+            {
+                //pure user defined sideband:
+                stg.PrepareAndSendData(2 * (uint)HeadStage + 1, sideband2, duration, STG_DestinationEnumNet.syncoutdata);
+            }
+            else
+            {
+                //alternative: adding sideband data for automatic stimulation mode:
+                CStimulusFunctionNet.SidebandData SidebandData2 = stg.Stimulus.CreateSideband(StimulusActive2, sideband2, duration, Bit0Time, Bit3Time, Bit4Time);
+                stg.PrepareAndSendData(2 * (uint) HeadStage + 1, SidebandData2.Sideband, SidebandData2.Duration, STG_DestinationEnumNet.syncoutdata);
+            }
+
+            stg.SetupTrigger(0, new uint[]{255}, new uint[]{255}, new uint[]{10});
+
+            stg.SendStart(1);
+        }
+
+        private void btStimulatinStop_Click(object sender, EventArgs e)
+        {
+            stg.SendStop(1);
+        }
+    }
+}
---
+++ SocketPrototypes/MEASocketConnection/Program.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using Mcs.Usb;
+
+namespace MEA2100_Recording_and_Stimulation
+{
+    static class Program
+    {
+        private static CMcsUsbListNet list = new CMcsUsbListNet(DeviceEnumNet.MCS_DEVICE_USB);
+        private static CMeaUSBDeviceNet dacq = new CMeaUSBDeviceNet();
+        private static int channelsInBlock = 0;
+        private static int threshold = 0;
+        private static List<int[]> currentData = new List<int[]>();
+        private static bool isRecording = false;
+
+        [STAThread]
+        static void Main()
+        {
+            Console.WriteLine("Initializing MCS device...");
+            InitializeMCSDevice();
+
+            // Define the IP address and port to listen on
+            string ipAddress = "127.0.0.1"; // Localhost
+            int port = 8080; // Port number
+
+            // Create a TCP/IP socket
+            Socket listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                // Bind the socket to the IP address and port
+                listenerSocket.Bind(new IPEndPoint(IPAddress.Parse(ipAddress), port));
+
+                // Start listening for incoming connections
+                listenerSocket.Listen(10); // Allow up to 10 pending connections
+
+                Console.WriteLine("Server started. Listening for incoming connections...");
+
+                // Accept incoming connections asynchronously
+                listenerSocket.BeginAccept(new AsyncCallback(AcceptCallback), listenerSocket);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+            }
+
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine();
+        }
+
+        static void InitializeMCSDevice()
+        {
+            // Set up event handlers
+            list.DeviceArrival += List_DeviceArrivalRemoval;
+            list.DeviceRemoval += List_DeviceArrivalRemoval;
+            dacq.ErrorEvent += Dacq_ErrorEvent;
+            dacq.ChannelDataEvent += Dacq_ChannelDataEvent;
+
+            // Initialize data list
+            for (int i = 0; i < 60; i++)
+            {
+                currentData.Add(new int[100]);
+            }
+
+            // Try to connect to the first available device
+            if (list.GetUsbListEntries().Length > 0)
+            {
+                StartRecording(list.GetUsbListEntries()[0]);
+            }
+            else
+            {
+                Console.WriteLine("No MCS devices found. Using simulated data.");
+            }
+        }
+
+        static void List_DeviceArrivalRemoval(CMcsUsbListEntryNet entry)
+        {
+            // Replace the IsConnected property with a check that works with your version
+            Console.WriteLine($"Device event: {entry}");
+        }
+
+        static void Dacq_ErrorEvent(string msg, int action)
+        {
+            Console.WriteLine($"MCS Error: {msg}");
+        }
+
+        static void Dacq_ChannelDataEvent(CMcsUsbDacqNet dacq, int CbHandle, int numFrames)
+        {
+            try
+            {
+                List<int[]> data = new List<int[]>();
+                for (int i = 0; i < channelsInBlock / 2; i++)
+                {
+                    data.Add(dacq.ChannelBlock.ReadFramesI32(i, 0, threshold, out int frames_ret));
+                }
+
+                // Update the current data with actual readings
+                lock (currentData)
+                {
+                    for (int i = 0; i < Math.Min(60, data.Count); i++)
+                    {
+                        int[] channelData = data[i];
+                        int[] bufferData = currentData[i];
+                        
+                        // Copy the latest 100 samples
+                        int startIdx = Math.Max(0, channelData.Length - 100);
+                        for (int j = 0; j < Math.Min(100, channelData.Length); j++)
+                        {
+                            int sourceIdx = startIdx + j;
+                            if (sourceIdx < channelData.Length)
+                            {
+                                bufferData[j] = channelData[sourceIdx];
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in ChannelDataEvent: {ex.Message}");
+            }
+        }
+
+        static void StartRecording(CMcsUsbListEntryNet deviceEntry)
+        {
+            try
+            {
+                uint status = dacq.Connect(deviceEntry);
+                if (status == 0)
+                {
+                    dacq.StopDacq(0); // if software had not stopped sampling correctly before
+                    CSCUFunctionNet scu = new CSCUFunctionNet(dacq);
+                    scu.SetDacqLegacyMode(false);
+                    int samplerate = 50000;
+                    dacq.SetSamplerate(samplerate, 0, 0);
+                    dacq.SetDataMode(DataModeEnumNet.Signed_32bit, 0);
+
+                    // For MEA2100-Mini it is assumed that only one HS is connected
+                    dacq.SetNumberOfAnalogChannels(60, 0, 0, 8, 0);
+                    dacq.EnableDigitalIn(DigitalDatastreamEnableEnumNet.DigitalIn | DigitalDatastreamEnableEnumNet.DigitalOut |
+                        DigitalDatastreamEnableEnumNet.Hs1SidebandLow | DigitalDatastreamEnableEnumNet.Hs1SidebandHigh, 0);
+                    dacq.EnableChecksum(true, 0);
+
+                    // Get channel layout
+                    dacq.GetChannelLayout(out int analogChannels, out int digitalChannels, out int checksumChannels, out int timestampChannels, out channelsInBlock, 0);
+
+                    int queuesize = samplerate;
+                    threshold = samplerate / 10;
+
+                    // channelsInBlock / 2 gives the number of channels in 32bit
+                    dacq.ChannelBlock.SetSelectedChannels(channelsInBlock / 2, queuesize, threshold, SampleSizeNet.SampleSize32Signed, SampleDstSizeNet.SampleDstSize32, channelsInBlock);
+                    dacq.ChannelBlock.SetCommonThreshold(threshold);
+                    dacq.ChannelBlock.SetCheckChecksum((uint)checksumChannels, (uint)timestampChannels);
+
+                    dacq.StartDacq();
+                    isRecording = true;
+                    Console.WriteLine("Recording started successfully");
+                }
+                else
+                {
+                    Console.WriteLine("Connection failed: " + CMcsUsbNet.GetErrorText(status));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error starting recording: {ex.Message}");
+            }
+        }
+
+        static void AcceptCallback(IAsyncResult ar)
+        {
+            // Get the listener socket
+            Socket listenerSocket = (Socket)ar.AsyncState;
+
+            // End the asynchronous operation to accept the incoming connection
+            Socket clientSocket = listenerSocket.EndAccept(ar);
+
+            // Get the client's IP address and port
+            IPEndPoint clientEndPoint = (IPEndPoint)clientSocket.RemoteEndPoint;
+            Console.WriteLine($"Connected to client {clientEndPoint.Address}:{clientEndPoint.Port}");
+
+            // Get the current data
+            List<int[]> data;
+            lock (currentData)
+            {
+                data = new List<int[]>(currentData);
+            }
+
+            int bufferSize = 100;
+            int numChannels = 60;
+
+            // If not recording, fill with test pattern
+            if (!isRecording)
+            {
+                Console.WriteLine("Using test pattern (all 1s)");
+                data.Clear();
+                for (int i = 0; i < numChannels; i++)
+                {
+                    int[] channelData = new int[bufferSize];
+                    for (int j = 0; j < bufferSize; j++)
+                    {
+                        channelData[j] = 1;
+                    }
+                    data.Add(channelData);
+                }
+            }
+
+            // actual processing code
+            byte[] original = new byte[0];
+
+            for (int i = 0; i < numChannels; i++)
+            {
+                for (int j = 0; j < bufferSize; j++)
+                {
+                    byte[] end = BitConverter.GetBytes(data[i][j]);
+                    byte[] combined = new byte[original.Length + end.Length];
+                    Array.Copy(original, combined, original.Length);
+                    Array.Copy(end, 0, combined, original.Length, end.Length);
+                    original = combined;
+                }
+            }
+
+            clientSocket.Send(original);
+
+            // Close the client socket
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
+
+            // Continue listening for incoming connections
+            listenerSocket.BeginAccept(new AsyncCallback(AcceptCallback), listenerSocket);
+        }
+    }
+}
